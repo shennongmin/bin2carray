1027359051b83676e9e17d1ef45f2d1122a14030
shennongmin <wupangfee@gmail.com>
2017-07-30 16:03:35 +0800

    Optimize crlf and version print.

--- README.md
+++ README.md
@@ -21,8 +21,12 @@ For help, run:
     
     Example: convert hello.bin to hello.c
         bin2carray hello
+    
+    Version:
+        <x.y.z>
 
-## A Example
+
+## A example
 
 Now, if you create hello.bin via running:
 
@@ -32,8 +36,12 @@ After `bin2carray hello`, `hello.c` will be generated like below:
 
     /* Wherever you copy this generated array to, please preserve comments including this line below with it! */
     /* You are ONLY allowed to change the name of the array below! */
-    const unsigned char hello[6] = {
+    const unsigned char bin2carray_hello[6] = {
         /* The content of this array is generated from bin2carray package in npm */
         /* DO NOT TOUCH ANY BYTES! */
         0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x0a
     };
+
+## License
+
+[GPL-2.0](https://www.gnu.org/licenses/gpl-2.0.html)
--- main.js
+++ main.js
@@ -8,26 +8,36 @@
  * @author www.shennongmin.org
  */
 
-const fs = require("fs");
+const fs = require('fs');
 const util = require('util');
+const pkg = require('./package.json');
+const os = require('os');
 
 const argc = process.argv.length;
 const args = process.argv;
+const eol = os.EOL;
 
-if (argc !== 3 || args[2] === "--help") {
-    let usage = "Usage:\r\n";
-    usage += "    bin2carray <filename without .bin>\r\n";
-    usage += "\r\n";
-    usage += "Example: convert hello.bin to hello.c\r\n"
-    usage += "    bin2carray hello\r\n";
+if (argc !== 3 || args[2] === `--help`) {
+    let usage = `Usage:${eol}`;
+    usage += `    bin2carray <filename without .bin>${eol}`;
+    usage += `${eol}`;
+    usage += `Example: convert hello.bin to hello.c${eol}`
+    usage += `    bin2carray hello${eol}`;
+    usage += `${eol}`;
+    usage += `Version:${eol}    ${pkg.version}${eol}`;
 
     console.log(usage);
     process.exit(1);
 }
 
+if (args[2] === '--version' || args[2] === '-V') {
+    console.log(`bin2carray ${pkg.version}`)
+    process.exit(0);
+}
+
 const filename = args[2];
-const formatBin = filename + ".bin";
-const formatC = filename + ".c";
+const formatBin = filename + `.bin`;
+const formatC = filename + `.c`;
 
 /*
  * Read bin
@@ -37,7 +47,7 @@ console.log(`read ${formatBin} done with length ${file.length} byte(s)`);
 
 
 function addZero(str, length){
-    return new Array(length - str.length + 1).join("0") + str;
+    return new Array(length - str.length + 1).join(`0`) + str;
 }
 
 
@@ -46,7 +56,7 @@ function addZero(str, length){
  * Convert to C language array.
  *
  * @code
- *  const unsigned char <filename>[<length>] = {
+ *  const unsigned char bin2carray_<filename>[<length>] = {
  *      0x12, 0x34, 0xfe, 0xdc, 0xdd, 0xea, 0xff, 0xa7, 0x12, 0x34, 0xfe, 0xdc, 0xdd, 0xea, 0xff, 0xa7,
  *      0x12, 0x34, 0xfe, 0xdc, 0xdd, 0xea, 0xff, 0xa7, 0x12, 0x34, 0xfe, 0xdc, 0xdd, 0xea, 0xff, 0xa7,
  *      0x12, 0x34, 0xfe, 0xdc, 0xdd, 0xea, 0xff, 0xa7, 0x12, 0x34, 0xfe, 0xdc, 0xdd, 0xea, 0xff, 0xa7,
@@ -54,39 +64,39 @@ function addZero(str, length){
  *  };
  * @endcode
  */
-console.log("Converting...");
+console.log(`Converting...`);
 
-let headerContent = "";
+let headerContent = ``;
 
-headerContent += "/* Wherever you copy this generated array to, please preserve comments including this line below with it! */\r\n";
-headerContent += "/* You are ONLY allowed to change the name of the array below! */\r\n";
-headerContent += `const unsigned char ${filename}[${file.length}] = {\r\n`;
-headerContent += "    /* The content of this array is generated from bin2carray package in npm */\r\n";
-headerContent += "    /* DO NOT TOUCH ANY BYTES! */\r\n";
+headerContent += `/* Wherever you copy this generated array to, please preserve comments including this line below with it! */${eol}`;
+headerContent += `/* You are ONLY allowed to change the name of the array below! */${eol}`;
+headerContent += `const unsigned char bin2carray_${filename}[${file.length}] = {${eol}`;
+headerContent += `    /* The content of this array is generated from bin2carray package in npm */${eol}`;
+headerContent += `    /* DO NOT TOUCH ANY BYTES! */${eol}`;
 
 for(let i = 0; i < file.length; i += 1) {
     let rowIndex = i % 16;
     let isLast = i === file.length - 1;
 
     if (rowIndex === 0) {
-        headerContent += "    ";
+        headerContent += `    `;
     }
 
-    headerContent += "0x";
+    headerContent += `0x`;
     headerContent += addZero(file.readUInt8(i).toString(16), 2);
 
     if (rowIndex < 15 && !isLast) {
-        headerContent += ", ";
+        headerContent += `, `;
     } else if (rowIndex === 15 && !isLast) {
-        headerContent += ",\r\n";
+        headerContent += `,${eol}`;
     } else {
-        headerContent += "\r\n";
+        headerContent += `${eol}`;
     }
 }
-headerContent += "};\r\n";
+headerContent += `};${eol}`;
 
-console.log("Convert done");
+console.log(`Convert done`);
 
-console.log("Saving...");
+console.log(`Saving...`);
 fs.writeFileSync(formatC, headerContent);
 console.log(`Saved as ${formatC}`);
